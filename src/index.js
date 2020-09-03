cc04e2ad0edcd61b879576e96a6eff9940c8ea6d
unknown <sara.reidy.21@gmail.com>
2020-09-03 14:09:25 -0400

    redo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import ReactGA from 'react-ga'
-
-const trackingId = 'UA-166256756-1'
-
-
-ReactGA.initialize(trackingId)
-ReactGA.event({category: "App", action: "Loaded"})
 
 ReactDOM.render(
   <React.StrictMode>
